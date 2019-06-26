173a3f9c5bcaa1e1d5756461b4d96748df6582d6
Cort Spellman <spellman.cort@gmail.com>
2019-06-26 10:37:05 -0400

    Move colon, semicolon, hyphen, and shift keys

--- Cort-Model01-Firmware/Cort-Model01-Firmware.ino
+++ Cort-Model01-Firmware/Cort-Model01-Firmware.ino
@@ -171,17 +171,17 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (___,           Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick,  Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_Escape,    Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Minus,
+  (___,                   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick,          Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Escape,            Key_A, Key_S, Key_D, Key_F, Key_G,
+   LSHIFT(Key_Semicolon), Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftShift,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_Enter,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),    Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Backslash,   Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                    Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   LSHIFT(Key_Semicolon), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightShift,
+   M(MACRO_ANY),   Key_6, Key_7, Key_8,     Key_9,      Key_0,     LockLayer(NUMPAD),
+   Key_Backslash,  Key_Y, Key_U, Key_I,     Key_O,      Key_P,     Key_Equals,
+                   Key_H, Key_J, Key_K,     Key_L,      Key_Minus, Key_Quote,
+   Key_RightShift, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_Semicolon,
    ShiftToLayer(ARROW), Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
