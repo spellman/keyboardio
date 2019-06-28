9188026d03c0e61f9d0a2910ca66258233d8d7b2
Cort Spellman <spellman.cort@gmail.com>
2019-06-28 10:48:38 -0400

    Move shifts to outer lower corners and : and ; to inner

--- Cort-Model01-Firmware/Cort-Model01-Firmware.ino
+++ Cort-Model01-Firmware/Cort-Model01-Firmware.ino
@@ -174,14 +174,14 @@ KEYMAPS(
   (___,                   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick,          Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_Escape,            Key_A, Key_S, Key_D, Key_F, Key_G,
-   LSHIFT(Key_Semicolon), Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftShift,
+   Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, LSHIFT(Key_Semicolon),
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_Enter,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),   Key_6, Key_7, Key_8,     Key_9,      Key_0,     LockLayer(NUMPAD),
    Key_Backslash,  Key_Y, Key_U, Key_I,     Key_O,      Key_P,     Key_Equals,
                    Key_H, Key_J, Key_K,     Key_L,      Key_Minus, Key_Quote,
-   Key_RightShift, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_Semicolon,
+   Key_Semicolon, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightShift,
    ShiftToLayer(PUNCTUATION), Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
