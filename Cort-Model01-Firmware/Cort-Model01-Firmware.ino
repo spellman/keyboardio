81ae036a55df11b08eeaeb213e4152a899eb5901
Cort Spellman <spellman.cort@gmail.com>
2020-04-25 19:09:51 -0500

    Swap backtick and tab => tab in normal place

--- Cort-Model01-Firmware/Cort-Model01-Firmware.ino
+++ Cort-Model01-Firmware/Cort-Model01-Firmware.ino
@@ -172,7 +172,7 @@ KEYMAPS(
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
   (___,                   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick,          Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Tab,               Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Backtick,
    Key_Escape,            Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, LSHIFT(Key_Semicolon),
    Key_LeftAlt, Key_Backspace, Key_LeftGui, Key_LeftControl,
@@ -189,7 +189,7 @@ KEYMAPS(
 
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
+   Key_Tab,      Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Backtick,
    Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
    Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
@@ -206,7 +206,7 @@ KEYMAPS(
 
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
+   Key_Tab,      Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Backtick,
    Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_D,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
@@ -223,7 +223,7 @@ KEYMAPS(
   // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Backtick,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
